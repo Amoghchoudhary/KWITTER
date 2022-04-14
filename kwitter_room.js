67d6bd7c7d32acc18a49cfa3ddef759602f6b943
Amoghchoudhary <84228409+Amoghchoudhary@users.noreply.github.com>
2022-04-14 15:36:53 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,17 +1,16 @@
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-      apiKey: "AIzaSyDuAP83kKO5PP3AF1up4x1ftf08c__zYz4",
-      authDomain: "tic-tac-toe-c9e0f.firebaseapp.com",
-      databaseURL: "https://tic-tac-toe-c9e0f-default-rtdb.firebaseio.com",
-      projectId: "tic-tac-toe-c9e0f",
-      storageBucket: "tic-tac-toe-c9e0f.appspot.com",
-      messagingSenderId: "798929944736",
-      appId: "1:798929944736:web:9768a481d1413e19ce49c5"
+      apiKey: "AIzaSyAtPoCwNLAiy_VVv3CZypzB-j-SDlGYgZI",
+      authDomain: "kwitter-4a8bb.firebaseapp.com",
+      databaseURL: "https://kwitter-4a8bb-default-rtdb.firebaseio.com",
+      projectId: "kwitter-4a8bb",
+      storageBucket: "kwitter-4a8bb.appspot.com",
+      messagingSenderId: "267681042026",
+      appId: "1:267681042026:web:c7047e76876b4bd1898413"
     };
     
     // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
+    firebase.initializeApp(firebaseConfig);
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
